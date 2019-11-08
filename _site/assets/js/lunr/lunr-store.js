d8412cd9a97151aa2b2de0639d29430707a08d05
Franco Calle <francocalle93@gmail.com>
2019-11-08 16:57:24 -0500

    update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Discrete Choice Models - Matlab",
-        "excerpt":"Package for estimation and simulation of discrete choice models [Matlab]Discrete choice models are becoming trendier as the availability of data increases in multiple areas like health, education, credit markets, and many others. The key idea of them is to model the decision making of an individual to choose a particular...","categories": [],
+        "excerpt":"Discrete choice models in MatlabClone my repo here.Discrete choice models are becoming trendier as more data becomes available in multiple areas like health, education, credit markets, and many others. The key idea of them is to model the decision making of an individual to choose a particular option among a...","categories": [],
         "tags": ["logit","discrete choice model","behavioral model"],
         "url": "http://localhost:4000/dcm/",
         "teaser":null}]
