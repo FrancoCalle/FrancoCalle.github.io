c3a9ed70a3cff4626ad75e8cfc94cebcd265107d
Franco Calle <francocalle93@gmail.com>
2019-11-12 20:23:20 -0500

    update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Discrete Choice Models - Matlab",
-        "excerpt":"Discrete choice models in MatlabClone my repo here.Discrete choice models are becoming trendier as more data becomes available in multiple areas like health, education, credit markets, and many others. The key idea of them is to model the decision making of an individual to choose a particular option among a...","categories": [],
+        "excerpt":"Discrete choice models in MatlabFork my repo here.Models available: Logit Exploded Logit Logit with Random Coefficients Exploded Logit with Random CoefficientsDiscrete choice models are becoming trendier as more data becomes available in multiple areas like health, education, credit markets, and many others. The key idea of them is to model...","categories": [],
         "tags": ["logit","discrete choice model","behavioral model"],
         "url": "http://localhost:4000/dcm/",
         "teaser":null}]
